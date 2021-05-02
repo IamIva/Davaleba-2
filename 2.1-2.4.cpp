719cdf3b9d960b6fc9e61ba367416ab18ea1a51d
IamIva <51024839+IamIva@users.noreply.github.com>
2021-05-02 22:01:45 +0400

    Add files via upload

---
+++ 2.1-2.4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+// Task 2.1
+//void changeValueP(int *p1, int *p2){
+//    *p1 *= 10;
+//    *p2 *= 10;
+//}
+// Task 2.4
+//int sum(int a, int b){
+//    return a + b;
+//}
+//int sub(int a, int b){
+//    return a - b;
+//}
+
+
+int main() {
+//   cout << "Task 2.1" << endl;
+//   int num1, num2;
+//   int* p1 = &num1;
+//   int* p2 = &num2;
+//   cin >> num1 >> num2;
+//   cout << "num1 = " << num1 << endl << "num2 = " << num2 << endl;
+//   changeValueP(p1, p2);
+//   cout << "num1 = " << num1 << endl << "num2 = " << num2 << endl;
+//
+
+//   cout << "Task 2.2" << endl;
+//   int a, b;
+//   while(a != 0){
+//        cin >> a;
+//        b += a;
+//   }
+//   cout << b;
+
+//
+//    cout << "Task 2.3" << endl;
+//    string s = "aba";
+//    for(auto e : s){
+//        for(auto ee: s){
+//            if(e != ee){
+//                cout << e << ee << endl;
+//            }
+//        }
+//    }
+
+
+
+    return 0;
+}
